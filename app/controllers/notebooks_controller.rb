9d806cd794464851a26f8ca8d9f337d850a4b69f
Jhonas Gomes <jhowscts@gmail.com>
2025-07-09 19:57:50 -0300

    feat: Adiciona funcionalidade de cadastro de notebooks

--- app/controllers/notebooks_controller.rb
+++ app/controllers/notebooks_controller.rb
@@ -6,4 +6,27 @@ class NotebooksController < ApplicationController
   def show
     @notebook = Notebook.find(params[:id])
   end
+
+  def new
+    @notebook = Notebook.new
+  end
+
+  def create
+    @notebook = Notebook.new(notebook_params)
+    @notebook.estado = 'disponível' # Define o estado padrão como 'Disponível'
+    if @notebook.save
+      redirect_to @notebook, notice: 'Notebook cadastrado com sucesso!.'
+    else
+      render :new, ststus: :unprocessable_entity
+    end
+  end
+
+  private
+  def notebook_params
+    params.require(:notebook).permit(
+      :marca, :modelo, :numero_patrimonio, :numero_serie,
+      :identificacao_equipamento, :data_compra, :data_fabricacao,
+      :descricao, :estado
+    )
+  end
 end
