bcc5a3df600fedd637ae0383a636bd8eacc5f595
Jhonas Gomes <jhowscts@gmail.com>
2025-07-10 01:50:23 -0300

    test: corrige testes do notebooks controller com helpers e dados corretos

--- test/controllers/notebooks_controller_test.rb
+++ test/controllers/notebooks_controller_test.rb
@@ -2,12 +2,26 @@ require "test_helper"
 
 class NotebooksControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get notebooks_index_url
+    get notebooks_url
     assert_response :success
   end
 
   test "should get show" do
-    get notebooks_show_url
+    # Cria um notebook para o teste
+    notebook = Notebook.create!(
+      marca: "Dell",
+      modelo: "XPS",
+      numero_patrimonio: "123",
+      numero_serie: "ABC123",
+      identificacao_equipamento: "Lab",
+      data_compra: Date.today,
+      data_fabricacao: Date.today,
+      descricao: "Test",
+      estado: "disponível",
+      foi_emprestado: false
+    )
+
+    get notebook_url(notebook)
     assert_response :success
   end
 end
